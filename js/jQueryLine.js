3d293e35453b4efeef148a539620b938858bfbfb
Gennadi Heimann <genaheimann@gmail.com>
2017-03-30 23:32:44 +0200

    2. elem add

--- js/jQueryLine.js
+++ js/jQueryLine.js
@@ -4,8 +4,9 @@ jQuery(document).ready(function($) {
         strokeWidth: 8,
         orientation: "auto",
         paths: [
-          { start: "#person_1", end: "#person_2", strokeWidth: 5, },
-          { start: "#person_1", end: "#person_3", strokeWidth: 5, }
+          { start: "#person_1", end: "#person_2", stroke: "#FE2E2E", strokeWidth: 5, orientation: "horizontal"  },
+          { start: "#person_1", end: "#person_3", stroke: "#01DF01", strokeWidth: 5, orientation: "horizontal" },
+          { start: "#person_4", end: "#person_2", stroke: "#0101DF", strokeWidth: 5, orientation: "horizontal" }
         ]
       });
     });
